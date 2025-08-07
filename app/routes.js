419ce6dfd6a632a452c551ba0ea15eeff4d593b1
stephbatliner <stephanie.batliner@digital.justice.gov.uk>
2025-08-07 11:00:34 +0100

    Onboarding interaction design changes

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,26 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+//Confirmation page: Next steps
+router.post('/confirmation', function (req, res) {
+    var POMadded = req.session.data['POMadded']
+    // Check whether the variable matches a condition
+    if (POMadded == "MakeAllocations"){
+     
+     // Send user to make allocations page
+      res.redirect('/allocations/final/make-allocations')
+    } else {
+      // Send user to onboarding POM search
+      res.redirect('/2025/onboarding/search')
+    }
+  });
+
+
+
+
+
+
+
+
+
