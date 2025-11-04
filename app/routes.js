a898fecd77fe9a9a4f3dfb4734f92628f5aca36d
stephbatliner <stephanie.batliner@digital.justice.gov.uk>
2025-11-04 12:54:59 +0000

    Bulk allocation interactions

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,8 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+//POM Onboarding
+
 //Confirmation page: Next steps
 router.post('/confirmation', function (req, res) {
     var POMadded = req.session.data['POMadded']
@@ -23,8 +25,21 @@ router.post('/confirmation', function (req, res) {
   });
 
 
- 
+//Bulk allocation
 
+//Confirmation page: Next steps
+router.post('/confirmationbulk', function (req, res) {
+  var BulkCasesAllocated = req.session.data['BulkCasesAllocated']
+  // Check whether the variable matches a condition
+  if (BulkCasesAllocated == "MoreBulkAllocations"){
+   
+   // Send user to make allocations page
+    res.redirect('/2025/bulkallocation/bulk-cases')
+  } else {
+    // Send user to onboarding POM search
+    res.redirect('/2025/staff/staff')
+  }
+});
 
 
 
